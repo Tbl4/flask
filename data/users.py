dfd38fd96bc0fbe886e726f06e85e7283a4f6cf3
Tbl4uHKa <ju789ooll@yandex.ru>
2021-04-23 10:59:51 +0600

    Чутка подправил

--- data/users.py
+++ data/users.py
@@ -3,11 +3,16 @@ import sqlalchemy
 from flask_login import UserMixin
 from sqlalchemy import orm
 
+from flasker import db
+from time import time
+import jwt
+import app
+
 from werkzeug.security import generate_password_hash, check_password_hash
 from .db_session import SqlAlchemyBase
 
 
-class User(SqlAlchemyBase, UserMixin):
+class User(SqlAlchemyBase, UserMixin, db.Model):
     __tablename__ = 'users'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
@@ -15,12 +20,27 @@ class User(SqlAlchemyBase, UserMixin):
     about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    is_admin = sqlalchemy.Column(sqlalchemy.BOOLEAN, default=False)
 
-    def __repr__(self):
-        return f'<Colonist> {self.id} {self.surname} {self.name}'
+    # def __repr__(self):
+    #     return f'{self.id} {self.email} {self.name} {self.is_admin}'
 
     def set_password(self, password):
         self.hashed_password = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.hashed_password, password)
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
